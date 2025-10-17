737a83c78d7dfef645d1960835cef71c6bddfafd
mike-jones <michaeldeitch04@gmail.com>
2025-10-17 00:52:00 -0700

    WIP -- Almost file -> JSON serialization
    There are currently some oddities, namely that the
    coord recording isn't really working correctly, and
    when ext is referenced, it's being named 'ext[0]' instead
    of just 'ext' in the json.
    
    TODO:
    - Fix the way in which coords are serialized
    - Remove the array access of the ext name (or anywhere else it's present)

--- ast_gen_refactor.py
+++ ast_gen_refactor.py
@@ -4,8 +4,10 @@
 #       AST      ->  Dict
 #       Dict     ->  JSON
 # And the reverse order ( JSON -> Dict -> AST)
+from pstats import SortKey
 
 import pycparser
+import json
 
 
 
@@ -24,4 +26,54 @@ def file_to_ast(file_name):
 '''Returns a dictionary representation of the AST
    Needed because json library can directly turn a dict to json format'''
 def node_to_dict(node_in_ast):
-    return "Coming soon..."
+    #Initialize a new dict for the ast
+    dict_representation = {}
+
+
+    #Record metadata of the node
+    dict_representation["_nodetype"] = node_in_ast.__class__.__name__
+
+
+    #Record Local node attribs
+    local_attribs = node_in_ast.attr_names
+
+    for attr in local_attribs:
+        dict_representation[attr] = getattr(node_in_ast, attr)
+
+
+    #Record Coord object (serialize first)
+    '''
+    if node_in_ast.coord is not None:
+        dict_representation["coord"] = node_in_ast.coord
+    else:
+        dict_representation["coord"] = None
+    '''
+
+    #Record child attribs (might come in the form of a dict, or array of dicts)
+    #TODO this child stuff isn't working correctly -- look into that lol
+
+    # for i in range(1, len(node_in_ast.children()) + 1):
+    #     child = node_in_ast.children()[0][i]
+    #     child_dict = node_to_dict(child)
+    #     dict_representation[node_in_ast.children()[0][i-1]] = child_dict
+
+
+    # approach for finding all child tuples
+    number_of_children = len(node_in_ast.children())
+    for i in range(0, number_of_children):
+        child_name = node_in_ast.children()[i][0]
+        child_dict = node_to_dict(node_in_ast.children()[i][1])
+        dict_representation[child_name] = child_dict
+
+    # for child in node_in_ast.children():
+    #     child_dict = node_to_dict(child)
+    #     dict_representation[child.name] = child_dict
+
+    return dict_representation
+
+
+if __name__ == "__main__":
+    ast = pycparser.parse_file('easy.c')
+    dictionary = node_to_dict(ast)
+    print(json.dumps(dictionary, sort_keys=True, indent=4))
+    balls = True
